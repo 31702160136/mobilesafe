3aa5add353eb83e62c264b0991788ccb14e8b3f7
杨鸿燊 <1012117138@qq.com>
2019-03-06 10:09:14 +0800

    Initial commit

---
+++ app/src/main/java/cn/mmvtc/mobilesafe/chapter01/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.mmvtc.mobilesafe.chapter01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.mmvtc.mobilesafe.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/cn/mmvtc/mobilesafe/chapter01/entity/VersionEntity.java
@@ -0,0 +1,15 @@
+package cn.mmvtc.mobilesafe.chapter01.entity;
+
+/**
+ * Created by Administrator on 2019/3/6.
+ */
+
+public class VersionEntity {
+    //服务器版本号
+    public String versioncode;
+    //版本描述
+    public String description;
+    //apk下载地址
+    public String apkurl;
+
+}
