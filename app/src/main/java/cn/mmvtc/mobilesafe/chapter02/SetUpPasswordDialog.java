4c80946a1c59e104cc5fb6e8c3612887b97a9ac8
杨鸿燊 <1012117138@qq.com>
2019-03-20 09:48:57 +0800

    1.4

--- app/src/main/java/cn/mmvtc/mobilesafe/chapter02/SetUpPasswordDialog.java
+++ app/src/main/java/cn/mmvtc/mobilesafe/chapter02/SetUpPasswordDialog.java
@@ -1,16 +1,27 @@
 package cn.mmvtc.mobilesafe.chapter02;
 
-import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.mmvtc.mobilesafe.R;
 
 
-public class SetUpPasswordDialog extends Activity {
+public class SetUpPasswordDialog extends Dialog implements View.OnClickListener{
+
+    public SetUpPasswordDialog(Context context) {
+        super(context);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_up_password_dialog);
+        setContentView(R.layout.set_up_password_dialog);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
